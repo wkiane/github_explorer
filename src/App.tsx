457977681c46ba70a36fe1cec6f3458739d5b4c3
wkiane <willa_abreu@outlook.com>
2020-04-29 00:18:03 -0300

    added global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyle from './styles/global';
+import Routes from './routes';
+
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
