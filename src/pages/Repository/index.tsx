93c05b476546baef0e2a40818b3fbe687f3a6031
wkiane <willa_abreu@outlook.com>
2020-04-30 17:48:03 -0300

    added styles to repository page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,71 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+
+import logo from '../../assets/logo.svg';
+import { Header, RepositoryInfo, Issues } from './styles';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="Github Explorer" />
+
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/28929274?v=4"
+            alt="Rocketseat"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Accusamus, natus.
+            </p>
+          </div>
+        </header>
+
+        <ul>
+          <li>
+            <strong>1280</strong>
+            <span>Stars</span>
+          </li>
+
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="djldsj">
+          <div>
+            <strong>repository.full_name</strong>
+            <p>repository.description</p>
+          </div>
+          <FiChevronRight />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
